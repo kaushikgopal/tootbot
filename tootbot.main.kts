a2e05aba334f11872e09205db6c51079fa5b150b
kaush <1042037+kaushikgopal@users.noreply.github.com>
2023-02-09 20:00:38 -0800

    add the ability to tweet as well

--- tootbot.main.kts
+++ tootbot.main.kts
@@ -7,6 +7,8 @@
 @file:DependsOn("com.squareup.moshi:moshi:1.13.0")
 @file:DependsOn("com.squareup.moshi:moshi-adapters:1.13.0")
 @file:DependsOn("com.squareup.moshi:moshi-kotlin:1.13.0")
+@file:DependsOn("org.twitter4j:twitter4j-core:4.0.7")
+
 
 @file:Repository("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 @file:DependsOn("social.bigbone:bigbone:2.0.0-SNAPSHOT")
@@ -16,6 +18,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.util.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okio.FileSystem
@@ -24,23 +27,28 @@ import org.joda.time.LocalDate
 import org.joda.time.LocalDateTime
 import social.bigbone.MastodonClient
 import social.bigbone.api.entity.Status
-import java.util.*
+import twitter4j.TwitterFactory
+import twitter4j.conf.ConfigurationBuilder
+
+val jsonFeedUrl = "https://kau.sh/index.json"
+val tootsFile = "./toots.csv".toPath()
+val forceTweet = listOf(
+    "",
+)
+val forceToot = listOf(
+    "",
+)
 
 val mastodonToken = ""
 val mastodonInstance = "mastodon.social"
-val jsonFeedUrl = "https://kau.sh/index.json"
-val tootsFile = "./toots.csv".toPath()
-val forceToot = listOf(
-    "blog/2023-02-02-own-your-online-presence/index.md",
-)
+
+val twitterConsumerKey = ""
+val twitterConsumerSecret = ""
+val twitterAccessToken = ""
+val twitterAccessTokenSecret = ""
+
 
 println(" *** 🤖Tootbot 🏁 *** ")
-
-/*
- * **********************
- * Load blog feed json
- * **********************
- */
 
 data class Page(
     val title: String,
@@ -60,7 +68,7 @@ data class Page(
 
         $url #blog
       """
-    }else {
+    } else {
       """
         $title
 
@@ -76,6 +84,18 @@ data class Feed(
     @Json(name = "items")
     val pages: List<Page>
 )
+
+data class Tooted(
+    val postId: String,
+    var tootId: String,
+    var tweetId: Long,
+)
+
+/*
+ * **********************
+ * Load blog feed json
+ * **********************
+ */
 
 // download RSS feed file
 var feed: Feed
@@ -103,22 +123,17 @@ println("🤖 found ${feed.pages.count()} pages in the feed")
  * **********************
  */
 
-data class Tooted(
-    val postId: String,
-    val tootId: String,
-)
-
-var tooted = mutableListOf<Tooted>()
+var tootedList = mutableListOf<Tooted>()
 
 FileSystem.SYSTEM.read(tootsFile) {
   while (true) {
     val line = readUtf8Line() ?: break
-    val (path, tootId) = line.split(',', ignoreCase = false, limit = 2)
-    tooted.add(Tooted(path, tootId))
+    val (path, tootId, tweetId) = line.split(',', ignoreCase = false, limit = 3)
+    tootedList.add(Tooted(path, tootId, tweetId.toLong()))
   }
 }
 
-println("🤖 tooted ${tooted.count()} times before")
+println("🤖 tooted ${tootedList.count()} times before")
 
 /*
  * **********************
@@ -126,15 +141,26 @@ println("🤖 tooted ${tooted.count()} times before")
  * **********************
  */
 
-val tootedFilePaths = tooted.map { it.postId }
-val tootable: List<Page> = feed.pages
-    .filterNot { page -> page.id in tootedFilePaths }
+val tootableList: List<Page> = feed.pages
     .filter { page ->
-      LocalDate(page.publishedDate) == LocalDateTime.now().toLocalDate() ||
-          page.id in forceToot
+      val notTootedBefore = tootedList.find { it.postId == page.id }?.tootId?.isBlank() ?: true
+      val publishedToday =
+          LocalDate(page.publishedDate) == LocalDateTime.now().toLocalDate()
+
+      page.id in forceToot || (notTootedBefore && publishedToday)
     }
 
-println("🤖 about to send ${tootable.count()} toots now")
+val tweetableList: List<Page> = feed.pages
+    .filter { page ->
+      val notTweetedBefore = tootedList.find { it.postId == page.id }?.tweetId == (0 ?: true)
+      val publishedToday =
+          LocalDate(page.publishedDate) == LocalDateTime.now().toLocalDate()
+
+      page.id in forceTweet || (notTweetedBefore && publishedToday)
+    }
+
+
+println("🤖 about to send ${tootableList.count()} toots now")
 
 /*
  * **********************
@@ -142,21 +168,21 @@ println("🤖 about to send ${tootable.count()} toots now")
  * **********************
  */
 
-val mastodonClient = MastodonClient.Builder(mastodonInstance)
-    .accessToken(mastodonToken)
-    .build()
+tootableList.forEach { page ->
+  val tootedStatus = Tooted(page.id, "", 0)
 
-//mastodonClient.timelines
-//  .getHomeTimeline(Range())
-//  .execute()
-//  .part
-//  .forEach {
-//    println("🐘 ${it.content}")
-//  }
-
-
-tootable.forEach { page ->
   try {
+    val mastodonClient = MastodonClient.Builder(mastodonInstance)
+        .accessToken(mastodonToken)
+        .build()
+
+    //mastodonClient.timelines
+    //  .getHomeTimeline(Range())
+    //  .execute()
+    //  .part
+    //  .forEach {
+    //    println("🐘 ${it.content}")
+    //  }
 
     val request = mastodonClient.statuses.postStatus(
         status = page.statusText(),
@@ -168,15 +194,40 @@ tootable.forEach { page ->
     )
 
     val status = request.execute()
+    tootedStatus.tootId = status.id
     println("🐘 posted status at ${status.id}")  // 109798419127349990
-    tooted.add(Tooted(page.id, status.id))
 
   } catch (e: Exception) {
     println("\uD83D\uDED1\uD83D\uDED1\uD83D\uDED1 error ${e.localizedMessage}")
   }
 
+  tootedList.add(tootedStatus)
 }
 
+tweetableList.forEach { page ->
+  val tootedStatus = tootedList.find { page.id == it.postId } ?: Tooted(page.id, "", 0)
+
+  try {
+    val twitterClient = TwitterFactory(
+        ConfigurationBuilder()
+            .setOAuthConsumerKey(twitterConsumerKey)
+            .setOAuthConsumerSecret(twitterConsumerSecret)
+            .setOAuthAccessToken(twitterAccessToken)
+            .setOAuthAccessTokenSecret(twitterAccessTokenSecret)
+            .build()
+    ).instance
+
+    val status = twitterClient.updateStatus(page.statusText())
+    tootedStatus.tweetId = status.id
+
+    println("🐥 posted status at ${status.id}")
+
+  } catch (e: Exception) {
+    println("\uD83D\uDED1\uD83D\uDED1\uD83D\uDED1 error ${e.localizedMessage}")
+  }
+
+  tootedList.add(tootedStatus)
+}
 
 /*
  * **********************
@@ -185,10 +236,12 @@ tootable.forEach { page ->
  */
 
 FileSystem.SYSTEM.write(tootsFile) {
-  tooted.forEach { tooted ->
+  tootedList.forEach { tooted ->
     writeUtf8(tooted.postId)
     writeUtf8(",")
     writeUtf8(tooted.tootId)
+    writeUtf8(",")
+    writeUtf8(tooted.tweetId.toString())
     writeUtf8("\n")
   }
 }
